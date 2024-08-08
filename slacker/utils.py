f08f562e4ece8a63a5c574ab24d048e19f1d8bba
azhou015 <andy.zhou@czbiohub.org>
2024-08-08 13:34:41 -0700

    simplified config structure and updated send_message to accept *args

--- slacker/utils.py
+++ slacker/utils.py
@@ -1,21 +1,16 @@
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import os
+from slacker.config import SLACK_BOT_TOKEN, CHANNEL_ID, BOT_NAME
 
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 # Initialize a WebClient instance with your OAuth token
 client = WebClient(token=SLACK_BOT_TOKEN)
 
-# The channel ID or name where you want to send the message
-channel_id = "CHANNEL-ID-HERE"
-
-# The bot name
-bot_name = "BOT-NAME-HERE"
-
-def send_message(message):
+def send_message(*args):
+    message = " ".join(map(str, args))
     try:
         # Use the chat.postMessage method to send a message to the channel
-        response = client.chat_postMessage(channel=channel_id, text=message,username=bot_name)
+        response = client.chat_postMessage(channel=CHANNEL_ID, text=message,username=BOT_NAME)
         print("Message sent successfully!")
     except SlackApiError as e:
         # Error handling in case the message fails to send
